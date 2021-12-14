86e0edb3fcaab01e42ae9470dc02dc15da919513
Nicola <nicolacalise3355@gmail.com>
2021-12-14 17:17:08 +0100

    first update

---
+++ Dizionario_Offline/Dizionario_Offline/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dizionario_Offline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
